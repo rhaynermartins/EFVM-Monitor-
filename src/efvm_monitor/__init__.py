229c253ef7434759172ddea4c466e698c2dd3ed2
rhaynermartins <rhayner2005@hotmail.com>
2026-08-27 18:12:38 -0300

    Cria pacote do monitor EFVM

---
+++ src/efvm_monitor/__init__.py
@@ -0,0 +1,7 @@
+"""Monitor de disponibilidade de passagens da EFVM."""
+
+from efvm_monitor.checker import AvailabilityResult, AvailabilityStatus, EFVMClient
+from efvm_monitor.config import Settings
+
+__all__ = ["AvailabilityResult", "AvailabilityStatus", "EFVMClient", "Settings"]
+__version__ = "0.1.0"
