ba42b9be21b8ff1e0e7bcb9d66c2a08bead8e6bc
rhaynermartins <rhayner2005@hotmail.com>
2026-08-29 13:43:14 -0300

    feat: use Vale TLS chain for portal requests

--- src/efvm_monitor/checker.py
+++ src/efvm_monitor/checker.py
@@ -13,7 +13,7 @@ from zoneinfo import ZoneInfo
 import httpx
 
 from efvm_monitor.config import Settings
-from efvm_monitor.network import verified_ssl_context
+from efvm_monitor.network import vale_ssl_context
 
 LOGGER = logging.getLogger(__name__)
 LOCAL_TIMEZONE = ZoneInfo("America/Sao_Paulo")
@@ -56,7 +56,7 @@ class EFVMClient:
             base_url=settings.base_url,
             timeout=settings.timeout_seconds,
             follow_redirects=False,
-            verify=verified_ssl_context(),
+            verify=vale_ssl_context(),
             headers={
                 "Accept": "application/json",
                 "Content-Type": "application/json",
